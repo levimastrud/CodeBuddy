6e5fac4b5fc962e996bc7e0e51dd90122ee83f06
Levi <levikeithmastrud@gmail.com>
2022-06-21 14:03:33 -0500

    added styling to make which course you are on more obvious

--- src/components/Progression/Progression.jsx
+++ src/components/Progression/Progression.jsx
@@ -25,15 +25,15 @@ function Progression() {
       <h1>Learn HTML: {progress}% Complete</h1>
       <progress max={100} value = {progress}></progress>
       <Link to='/intro'><h1> Intro </h1></Link>
-      {progress > 0 ? <Link to='/basic-elements'><h1> Basic Elements </h1></Link> : <h1> Basic Elements </h1>}
-      {progress > 10 ? <Link to='/lists'><h1> Lists </h1></Link> : <h1> Lists </h1>}
-      {progress > 20 ? <Link to='/images'><h1> Images </h1></Link> : <h1> Images </h1>}
-      {progress > 30 ? <Link to='/links'><h1> Links </h1></Link> : <h1> Links </h1>}
-      {progress > 40 ? <Link to='/styles'><h1> Styles </h1></Link> : <h1> Styles </h1>}
-      {progress > 50 ? <Link to='/forms'><h1> Forms </h1></Link> : <h1> Forms </h1>}
-      {progress > 60 ? <Link to='/buttons'><h1> Buttons </h1></Link> : <h1> Buttons </h1>}
-      {progress > 70 ? <Link to='/tables'><h1> Tables </h1></Link> : <h1> Tables </h1>}
-      {progress > 80 ? <Link to='/final-test'><h1> Final Test </h1></Link> : <h1> Final Test </h1>}
+      {progress > 0 ? <Link to='/basic-elements'><h1 className='ready'> Basic Elements </h1></Link> : <h1 className='not-ready'> Basic Elements </h1>}
+      {progress > 10 ? <Link to='/lists'><h1 className='ready'> Lists </h1></Link> : <h1 className='not-ready'> Lists </h1>}
+      {progress > 20 ? <Link to='/images'><h1 className='ready'> Images </h1></Link> : <h1 className='not-ready'> Images </h1>}
+      {progress > 30 ? <Link to='/links'><h1 className='ready'> Links </h1></Link> : <h1 className='not-ready'> Links </h1>}
+      {progress > 40 ? <Link to='/styles'><h1 className='ready'> Styles </h1></Link> : <h1 className='not-ready'> Styles </h1>}
+      {progress > 50 ? <Link to='/forms'><h1 className='ready'> Forms </h1></Link> : <h1 className='not-ready'> Forms </h1>}
+      {progress > 60 ? <Link to='/buttons'><h1 className='ready'> Buttons </h1></Link> : <h1 className='not-ready'> Buttons </h1>}
+      {progress > 70 ? <Link to='/tables'><h1 className='ready'> Tables </h1></Link> : <h1 className='not-ready'> Tables </h1>}
+      {progress > 80 ? <Link to='/final-test'><h1 className='ready'> Final Test </h1></Link> : <h1 className='not-ready'> Final Test </h1>}
     </div>
   );
 }
