0e516fb98d50f9bd9418d7da3d85b8dcfbf95b71
Levi <levikeithmastrud@gmail.com>
2022-06-24 16:25:43 -0500

    tables topic complete

--- src/components/LearnHTML/Topics/FinalTest/Congratulations.jsx
+++ src/components/LearnHTML/Topics/FinalTest/Congratulations.jsx
@@ -7,30 +7,61 @@ function Congratulations
 () {
     const user = useSelector((store) => store.user);
     const history = useHistory()
-    return (
-        <div className="container">
-            <h1>Final Test</h1>
-            <p>“Yes?” I say. “Timothy?”“You’re a dufus.”“Oh leave Patrick alone,” Evelyn says. “He’s the boy next door.That’s Patrick. You’re not a dufus, are you, honey?” Evelyn is on Marsand I move toward the bar to make myself another drink.“Boy next door.” Tim smirks and nods, then reverses his expressionand hostilely asks Evelyn again if she has a lint brush.Evelyn finishes opening the Japanese beer bottles and tellsCourtney to fetch Stash and Vanden. “We have to eat this now or elsewe’re going to be poisoned,” she murmurs, slowly moving her head,taking in the kitchen, making sure she hasn’t forgotten anything.“If I can tear them away from the latest Megadeth video,” Courtneysays before exiting.“I have to talk to you,” Evelyn says.“What about?” I come up to her.“No,” she says and then pointing at Tim, “to Price.”Tim still glares at her fiercely. I say nothing and stare at Tim’sdrink.“Be a hon,” she tells me, “and place the sushi on the table. Tempurais in the microwave and the sake is just about done boiling.…” Hervoice trails off as she leads Price out of the kitchen.I am wondering where Evelyn got the sushi—the tuna, yellowtail,mackerel, shrimp, eel, even
-                bonito
-                , all seem so fresh and there arepiles of wasabi and clumps of ginger placed strategically around theWilton platter—but I also like the idea that I
-                don’t
-                know, will
-                never
-                know, will never
-                ask
-                where it came from and that the sushi will sitthere in the middle of the glass table from Zona that Evelyn’s fatherbought her like some mysterious apparition from the Orient and as Iset the platter down I catch a glimpse of my reflection on the surfaceof the table. My skin seems darker because of the candlelight and Inotice how good the haircut I got at Gio’s last Wednesday looks. Imake myself another drink. I worry about the sodium level in the soysauce.Four of us sit around the table waiting for Evelyn and Timothy toreturn from getting Price a lint brush. I sit at the head taking largeswallows of J&B. Vanden sits at the other end reading disinterestedlyfrom some East Village rag called
-                Deception
-                , its glaring headline
-                THE
 
-                DEATH OF DOWNTOWN
-                . Stash has pushed a chopstick into a lone piece of yellowtail that lies on the middle of his plate like some shiny impaledinsect and the chopstick stands straight up. Stash occasionally movesthe piece of sushi around the plate with the chopstick but never looksup toward either myself or Vanden or Courtney, who sits next to mesipping plum wine from a champagne glass.Evelyn and Timothy come back perhaps twenty minutes after we’veseated ourselves and Evelyn looks only slightly flushed. Tim glares atme as he takes the seat next to mine, a fresh drink in hand, and heleans over toward me, about to say, to admit something, whensuddenly Evelyn interrupts, “Not there, Timothy,” then, barely awhisper, “Boy girl, boy girl.” She gestures toward the empty chairnext to Vanden. Timothy shifts his glare to Evelyn and hesitantly takesthe seat next to Vanden, who yawns and turns a page of hermagazine.“Well, everybody,” Evelyn says, smiling, pleased with the meal shehas presented, “dig in,” and then after noticing the piece of sushi thatStash has pinned—he’s now bent low over the plate, whispering at it—her composure falters but she smiles bravely and chirps, “Plumwine anyone?”No one says anything until Courtney, who is staring at Stash’s plate,lifts her glass uncertainly and says, trying to smile, “It’s … delicious,Evelyn.”Stash doesn’t speak. Even though he is probably uncomfortable atthe table with us since he looks nothing like the other men in theroom—his hair isn’t slicked back, no suspenders, no horn-rimmedglasses, the clothes black and ill-fitting, no urge to light and suck on acigar, probably unable to secure a table at Camols, his net worth apittance—still, his behavior lacks warrant and he sits there as if hypnotized by the glistening piece of sushi and just as the table isabout to finally ignore him, to look away and start eating, he sits upand loudly says, pointing an accusing finger at his plate, “It moved!”Timothy glares at him with a contempt so total that I can’t fullyequal it but I muster enough energy to come close. Vanden seemsamused and so now, unfortunately, does Courtney, who I’m beginningto think finds this monkey attractive but I suppose if I were datingLuis Carruthers I might too. Evelyn laughs good-naturedly and says,“Oh Stash, you
-                are
-                a riot,” and then asks worriedly, “Tempura?”</p>
+    let quizOptions = {
+        topic: 'forms_results',
+        router: 'forms',
+        nextTopic: 7,
+        // First Question
+        question1: 'What is a good way to tell the user what to put in an input?',
+        o1: {question: 'Labels', value: 1},
+        o2: {question: `With code comments`, value: 0},
+        o3: {question: 'With styline', value: 0},
+        o4: {question: 'Placeholders', value: 1},
+        // Second Question
+        question2: 'What do we have to give an input in order to use labels?',
+        o5: {question: `Class`, value: 0},
+        o6: {question: `Label`, value: 0},
+        o7: {question: 'Id', value: 1},
+        o8: {question: 'Href', value: 0},
+        // Third Question
+        question3: 'What type of tags go inside a form to allow user input?',
+        o9: {question:`<input>`, value: 1},
+        o10: {question: `<data>`, value: 0},
+        o11: {question: `<href>`, value: 0},
+        o12: {question: `<submit>`, value: 0},
+        // Fourth Question
+        question4: 'Which form is written correctly?',
+        o13: {question: `<input> <label> </label> </input>`, value: 0},
+        o14: {question: '<form> <label> </label> </form>', value: 0},
+        o15: {question: `<form> <input> </input> </form>`, value: 1},
+        o16: {question: '<form> <input> <input> <form>', value: 0},
+        // Fifth Question
+        question5: 'Inputs have a closing tag.',
+        o17: {question: 'True', value: 0},
+        o18: {question: 'False', value: 1},
+    }
+
+    const isReady = () => {
+        if (answer === 'Correct') {
+            dispatch({ type: 'SET_QUIZ', payload: quizOptions });
+            dispatch({ type: 'SET_ANSWER', payload: '' });
+            history.push('/quiz-page-one');
+        } else {
+            console.log('stay here')
+        }
+    }
+    
+    return (
+        <div className="quiz">
+            <h1>Congratulations, {user.name}!</h1>
+            <h3>Code Buddy is so proud of you!</h3>
+            <h3>Only one final test separates you from completing the Learn HTML course!</h3>
+            <h3>Good luck! You got it!</h3>
             <button onClick={() => {
                  // Stops user from backwards progression
                 user.recent_topic_completed > 10 ? '' : axios.post('/api/user/next-topic', {username: user.username, nextTopic: 10})
-                history.push('/progression')
+                history.push('/final-test')
                 }
             }
             >Finish Topic</button>
--- src/components/LearnHTML/Topics/Tables/TableBorders.jsx
+++ src/components/LearnHTML/Topics/Tables/TableBorders.jsx
@@ -1,39 +1,195 @@
 import axios from 'axios';
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Lesson from '../../Lesson/Lesson';
+import Quiz from '../../Quiz/QuizPageOne';
 
 function TableBorders() {
     const user = useSelector((store) => store.user);
+    const answer = useSelector((store) => store.answer);
     const history = useHistory()
-    return (
-        <div className="container">
-            <h1>TableBorders</h1>
-            <p>“Yes?” I say. “Timothy?”“You’re a dufus.”“Oh leave Patrick alone,” Evelyn says. “He’s the boy next door.That’s Patrick. You’re not a dufus, are you, honey?” Evelyn is on Marsand I move toward the bar to make myself another drink.“Boy next door.” Tim smirks and nods, then reverses his expressionand hostilely asks Evelyn again if she has a lint brush.Evelyn finishes opening the Japanese beer bottles and tellsCourtney to fetch Stash and Vanden. “We have to eat this now or elsewe’re going to be poisoned,” she murmurs, slowly moving her head,taking in the kitchen, making sure she hasn’t forgotten anything.“If I can tear them away from the latest Megadeth video,” Courtneysays before exiting.“I have to talk to you,” Evelyn says.“What about?” I come up to her.“No,” she says and then pointing at Tim, “to Price.”Tim still glares at her fiercely. I say nothing and stare at Tim’sdrink.“Be a hon,” she tells me, “and place the sushi on the TableBorders. Tempurais in the microwave and the sake is just about done boiling.…” Hervoice trails off as she leads Price out of the kitchen.I am wondering where Evelyn got the sushi—the tuna, yellowtail,mackerel, shrimp, eel, even
-                bonito
-                , all seem so fresh and there arepiles of wasabi and clumps of ginger placed strategically around theWilton platter—but I also like the idea that I
-                don’t
-                know, will
-                never
-                know, will never
-                ask
-                where it came from and that the sushi will sitthere in the middle of the glass TableBorders from Zona that Evelyn’s fatherbought her like some mysterious apparition from the Orient and as Iset the platter down I catch a glimpse of my reflection on the surfaceof the TableBorders. My skin seems darker because of the candlelight and Inotice how good the haircut I got at Gio’s last Wednesday looks. Imake myself another drink. I worry about the sodium level in the soysauce.Four of us sit around the TableBorders waiting for Evelyn and Timothy toreturn from getting Price a lint brush. I sit at the head taking largeswallows of J&B. Vanden sits at the other end reading disinterestedlyfrom some East Village rag called
-                Deception
-                , its glaring headline
-                THE
+    const dispatch = useDispatch();
 
-                DEATH OF DOWNTOWN
-                . Stash has pushed a chopstick into a lone piece of yellowtail that lies on the middle of his plate like some shiny impaledinsect and the chopstick stands straight up. Stash occasionally movesthe piece of sushi around the plate with the chopstick but never looksup toward either myself or Vanden or Courtney, who sits next to mesipping plum wine from a champagne glass.Evelyn and Timothy come back perhaps twenty minutes after we’veseated ourselves and Evelyn looks only slightly flushed. Tim glares atme as he takes the seat next to mine, a fresh drink in hand, and heleans over toward me, about to say, to admit something, whensuddenly Evelyn interrupts, “Not there, Timothy,” then, barely awhisper, “Boy girl, boy girl.” She gestures toward the empty chairnext to Vanden. Timothy shifts his glare to Evelyn and hesitantly takesthe seat next to Vanden, who yawns and turns a page of hermagazine.“Well, everybody,” Evelyn says, smiling, pleased with the meal shehas presented, “dig in,” and then after noticing the piece of sushi thatStash has pinned—he’s now bent low over the plate, whispering at it—her composure falters but she smiles bravely and chirps, “Plumwine anyone?”No one says anything until Courtney, who is staring at Stash’s plate,lifts her glass uncertainly and says, trying to smile, “It’s … delicious,Evelyn.”Stash doesn’t speak. Even though he is probably uncomforTableBorders atthe TableBorders with us since he looks nothing like the other men in theroom—his hair isn’t slicked back, no suspenders, no horn-rimmedglasses, the clothes black and ill-fitting, no urge to light and suck on acigar, probably unable to secure a TableBorders at Camols, his net worth apittance—still, his behavior lacks warrant and he sits there as if hypnotized by the glistening piece of sushi and just as the TableBorders isabout to finally ignore him, to look away and start eating, he sits upand loudly says, pointing an accusing finger at his plate, “It moved!”Timothy glares at him with a contempt so total that I can’t fullyequal it but I muster enough energy to come close. Vanden seemsamused and so now, unfortunately, does Courtney, who I’m beginningto think finds this monkey attractive but I suppose if I were datingLuis Carruthers I might too. Evelyn laughs good-naturedly and says,“Oh Stash, you
-                are
-                a riot,” and then asks worriedly, “Tempura?”</p>
-            <button onClick={() => {
-                 // Stops user from backwards progression
-                user.recent_topic_completed > 9 ? '' : axios.post('/api/user/next-topic', {username: user.username, nextTopic: 9})
-                history.push('/progression')
-                }
+    const checkAnswer = (codeBlock) => {
+
+        const parser = new DOMParser();
+        const parsed = parser.parseFromString(codeBlock, "text/html");
+        let parsedExpanded = parsed.lastChild.lastChild.childNodes;
+
+        // Logs used for programming check answer logic 
+
+        let wtfString = parsedExpanded[1].textContent.replace(/\r?\n/g, '');
+
+        console.log('parse expanded length:', parsedExpanded.length)
+        console.log('parse expanded:', parsedExpanded)
+        console.log('parsed', parsed.lastChild.lastChild)
+        console.log('Parsed Node Name goes here ->',)
+        console.log('WTF:', wtfString)
+        console.log('Parsed Node Name goes here ->', wtfString.search('border'))
+
+        // Determines whether or not codeBlock is correct.
+        // This will need to be modified for every different topic.
+
+        try {
+            if (parsedExpanded.length < 3) {
+                dispatch({ type: 'SET_ANSWER', payload: 'Incorrect' });
             }
-            >Finish Topic</button>
-        </div >
+            if ( wtfString.search('border') > 1 && wtfString.search('collapse') > 1 && wtfString.search('table') > 1) {
+                dispatch({ type: 'SET_ANSWER', payload: 'Correct' });
+            }
+             else {
+                dispatch({ type: 'SET_ANSWER', payload: 'Incorrect' });
+            }
+        } catch {
+            dispatch({ type: 'SET_ANSWER', payload: 'Incorrect' });
+        }
+    }
+
+    let lesson = `Typically tables we look at have borders. So in our CSS file we should be able to:
+    table {
+    border: 1 px solid black;
+    }
+    Right?
+    Well yes. But by default, borders on tables will apply a border to the whole table, as well as each cell.
+    To avoid this, we just need to set the ‘border-collapse’ property to ‘collapse’.           
+    `;
+
+    let task = `Add a border to our table and give it the border collapse!`;
+
+    let defaultAnswer = `
+    <html>
+        <head>
+        </head>
+        <body>
+            <style>
+
+                <!-- YOUR CODE HERE -->
+
+            </style>
+            <table>
+                <thead>
+                    <tr>
+                        <th> First Name </th>
+                        <th> Last Name </th >
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> ${user.name} </td>
+                        <td> Hackerson </td>
+                    </tr>
+                </tbody>
+            </table>
+        </body>
+    </html>
+    `;
+
+    let viewSolution = `
+    <html>
+        <head>
+        </head>
+        <body>
+            <style>
+                table, tr, th, td {
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                    padding: 15px;
+                }
+            </style>
+            <table>
+                <thead>
+                    <tr>
+                        <th> First Name </th>
+                        <th> Last Name </th >
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td> ${user.name} </td>
+                        <td> Hackerson </td>
+                    </tr>
+                </tbody>
+            </table>
+        </body>
+    </html>
+    `;
+
+    let hint = `Did you target table within the <style> tag?`
+
+    let quizOptions = {
+        topic: 'tables_results',
+        router: 'tables',
+        nextTopic: 9,
+        // First Question
+        question1: 'Which tag is used to create a table?',
+        o1: {question: '<thead>', value: 0},
+        o2: {question: `<table>`, value: 1},
+        o3: {question: '<td>', value: 0},
+        o4: {question: '<tr>', value: 0},
+        // Second Question
+        question2: 'How do you create a table row?',
+        o5: {question: `<row>`, value: 0},
+        o6: {question: `<th>`, value: 0},
+        o7: {question: '<tr>', value: 1},
+        o8: {question: '<td>', value: 0},
+        // Third Question
+        question3: 'What does <thead> and <tbody> do?',
+        o9: {question:`Adds a row to a table`, value: 0},
+        o10: {question: `Creates header and body of table`, value: 0},
+        o11: {question: `Organizes the header and body sections of a table`, value: 1},
+        o12: {question: `Removes the border from a table`, value: 0},
+        // Fourth Question
+        question4: 'Why would you collapse the borders?',
+        o13: {question: `Because by default tables have a border on the table cell aswell`, value: 1},
+        o14: {question: 'In case you wanted the table to have rounded corners', value: 0},
+        o15: {question: `If you wanted to change the background color of a table`, value: 0},
+        o16: {question: 'Because some men just want to watch borders collapse.', value: 0},
+        // Fifth Question
+        question5: 'Which is the proper order of elements?',
+        o17: {question: 'Table -> Tr -> Thead -> Tr', value: 0},
+        o18: {question: 'Table -> Thead -> Tr -> Td', value: 1},
+        o19: {question: 'Table -> Tr -> Thead -> Tbody', value: 0},
+        o20: {question: 'Table -> Thead -> Tr -> Th', value: 1}
+    }
+
+    const isReady = () => {
+        if (answer === 'Correct') {
+            dispatch({ type: 'SET_QUIZ', payload: quizOptions });
+            dispatch({ type: 'SET_ANSWER', payload: '' });
+            history.push('/quiz-page-one');
+        } else {
+            console.log('stay here')
+        }
+    }
+
+    useEffect(() => {
+        dispatch({ type: 'SET_CODE_BLOCK', payload: defaultAnswer });
+    }, [dispatch]);
+
+    return (
+        <>
+            <div className='topic'>
+                <h1> Labels </h1>
+                <Lesson
+                    defaultAnswer={defaultAnswer}
+                    viewSolution={viewSolution}
+                    lesson={lesson}
+                    hint={hint}
+                    task={task}
+                    checkAnswer={checkAnswer}
+                />
+                <button onClick={() => {
+                    // Stops user from backwards progression
+                    history.push('/progression')
+                }
+                }
+                > Back </button>
+                <button onClick={() => {
+                    isReady();
+                }}>Next</button>
+            </div >
+        </>
     );
 }
 
