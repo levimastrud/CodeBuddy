8a77e38a29efcbbc36302e160dd574fc484d4ada
Levi <levikeithmastrud@gmail.com>
2022-06-28 17:52:03 -0500

    graduation graphic now goes in the nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,17 +4,19 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
 import CB_Text from '../LearnHTML/CodeBuddy Graphics/CB_Text.svg'
-import CB_Logo from '../LearnHTML/CodeBuddy Graphics/CB_Logo.svg'
+import CB_GraduationText from '../LearnHTML/CodeBuddy Graphics/CB_GraduationText.svg'
+
 
 function Nav() {
   const user = useSelector((store) => store.user);
+  let progress = user.recent_topic_completed * 10;
   const history = useHistory();
 
   return (
     <div className="nav">
 
       <Link to="/home">
-        <img src={CB_Text} className="nav-title" />
+        <img src={progress === 100 ? CB_GraduationText : CB_Text } className="nav-title" />
       </Link>
       <div>
         {/* If no user is logged in, show these links */}
